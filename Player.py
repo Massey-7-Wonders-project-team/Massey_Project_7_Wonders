6f3fe236e751747e3a44c39e8af8cfe6adb44b29
jacobstringer <jacobstringer@windowslive.com>
2017-07-23 11:57:16 +1200

    Update Player.py

--- Player.py
+++ Player.py
@@ -3,30 +3,10 @@ class Player:
   def __init__ (self, name, position):
     self.name = name #necessary?
     self.position = position #order in which created, used for determining neighbours 
-    self.money = 3
-    self.points = 0
-    self.military = 0
-    self.played = [] # To add played cards into
-    # Materials
-    self.wood = 0
-    self.brick = 0
-    self.stone = 0
-    self.ore = 0
-    self.glass = 0
-    self.cloth = 0
-    self.paper = 0
-    # Green Research Cards
-    self.cog = 0
-    self.tablet = 0
-    self.compass = 0
-    self.wildcard = 0
-    # Other card counters <- I believe this makes dealing with the effects of more complex cards easier
-    self.yellowCard = 0 #commerce
-    self.brownCard = 0 #basic resource
-    self.blueCard = 0 #architecture
-    self.purpleCard = 0 #guild
-    self.redCard = 0 #military
-    self.greenCard = 0 #research
-    self.greyCard = 0 #advanced resource
+    self.state = [0, 0, 3, 0, 0, 0, 0, 0, 0, 0] # [Points, Military, Money, Wood, Brick, Ore, Stone, Glass, Paper, Cloth]
+
+    # Card counters <- I believe this makes dealing with the effects of more complex cards easier
+    self.cards = [0] * 10 # [yellow/commerce, brown/resource, blue/architecture, purple/guild, red/military, grey/advanced resources, 
+                          #  green cog, green tablet, green compass, green wildcard]
   
   # hands - do players hold hands, or are hands assigned to players? Probably the second is a faster implementation?
