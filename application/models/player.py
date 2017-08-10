899db5a0afac9c36dad6891f69c9fc908cd42904
jacobstringer <jacobstringer@windowslive.com>
2017-08-10 17:28:00 +1200

    Add files via upload

--- application/models/player.py
+++ application/models/player.py
@@ -1,10 +1,16 @@
 from index import db
+from .game import Game
+from .user import User
 
 
 class Player(db.Model):
     __tablename__ = 'player'
     gameId = db.Column(db.Integer, db.ForeignKey('game.id'), primary_key=True)
     userId = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    #game = db.relationship(Game, foreign_keys=[gameId])
+    #user = db.relationship(User, foreign_keys=[userId])
+
+    ready = db.Column(db.Boolean, default=False)
 
     wood = db.Column(db.Integer, default=0)
     brick = db.Column(db.Integer, default=0)
@@ -16,3 +22,7 @@ class Player(db.Model):
     points = db.Column(db.Integer, default=0)
     military = db.Column(db.Integer, default=0)
     money = db.Column(db.Integer, default=3)
+
+    def player(self, gameId, userId):
+        self.gameId = gameId
+        self.userId = userId
