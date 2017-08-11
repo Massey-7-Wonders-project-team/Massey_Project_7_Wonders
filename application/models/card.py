0267f08afff4bcfddb3b29add38c8cf69035df2c
Sam-Irv <samuelkyleirvine@gmail.com>
2017-08-11 12:14:49 +1200

    Update card.py

--- application/models/card.py
+++ application/models/card.py
@@ -5,8 +5,8 @@ class Card(db.Model):
     __tablename__ = 'card'
     name = db.Column(db.String(50), primary_key=True)
     noPlayers = db.Column(db.Integer, primary_key=True)
-    age = db.Column(db.Integer, default=1)
-    type = db.Column(db.String(10))
+    age = db.Column(db.Integer, primary_key=True)
+    colour = db.Column(db.String(10))
 
     costMoney = db.Column(db.Integer, default=0)
     costWood = db.Column(db.Integer, default=0)
@@ -16,8 +16,8 @@ class Card(db.Model):
     costGlass = db.Column(db.Integer, default=0)
     costPaper = db.Column(db.Integer, default=0)
     costCloth = db.Column(db.Integer, default=0)
-    prerequisite1 = db.Column(db.String(30), default='')
-    prerequisite2 = db.Column(db.String(30), default='')
+    prerequisite1 = db.Column(db.String(50), default='')
+    prerequisite2 = db.Column(db.String(50), default='')
 
     giveWood = db.Column(db.Integer, default=0)
     giveBrick = db.Column(db.Integer, default=0)
@@ -32,54 +32,54 @@ class Card(db.Model):
     giveMoney = db.Column(db.Integer, default=0)
     giveResearch = db.Column(db.String(10), default='')
     
-    def __init__(self, name, noPlayers, age, type):
+    def __init__(self, name, noPlayers, age, colour):
         self.name = name
         self.noPlayers = noPlayers
         self.age = age
-        self.type = type
+        self.colour = colour
     
-    def add_cost_money(self, money):
+    def set_cost_money(self, money):
         self.costMoney = money
-    def add_cost_wood(self, wood):
+    def set_cost_wood(self, wood):
         self.costWood = wood
-    def add_cost_brick(self, brick):
+    def set_cost_brick(self, brick):
         self.costBrick = brick
-    def add_cost_ore(self, ore):
+    def set_cost_ore(self, ore):
         self.costOre = ore
-    def add_cost_stone(self, stone):
+    def set_cost_stone(self, stone):
         self.costStone = stone
-    def add_cost_glass(self, glass):
+    def set_cost_glass(self, glass):
         self.costGlass = glass
-    def add_cost_paper(self, paper):
+    def set_cost_paper(self, paper):
         self.costPaper = paper
-    def add_cost_cloth(self, cloth):
+    def set_cost_cloth(self, cloth):
         self.costCloth = cloth
-    def add_prerequisite_1(self, prereq):
+    def set_prerequisite_1(self, prereq):
         self.prerequisite1 = prereq
-    def add_prerequisite_2(self, prereq):
+    def set_prerequisite_2(self, prereq):
         self.prerequisite2 = prereq
     
-    def add_benefit_wood(self, wood):
+    def set_benefit_wood(self, wood):
         self.giveWood = wood
-    def add_benefit_brick(self, brick):
+    def set_benefit_brick(self, brick):
         self.giveBrick = brick
-    def add_benefit_ore(self, ore):
+    def set_benefit_ore(self, ore):
         self.giveOre = ore
-    def add_benefit_stone(self, stone):
+    def set_benefit_stone(self, stone):
         self.giveStone = stone
-    def add_benefit_glass(self, glass):
+    def set_benefit_glass(self, glass):
         self.giveGlass = glass
-    def add_benefit_paper(self, paper):
+    def set_benefit_paper(self, paper):
         self.givePaper = paper
-    def add_benefit_cloth(self, cloth):
+    def set_benefit_cloth(self, cloth):
         self.giveCloth = cloth
     def set_resource_alternating(self, alt):
         self.resourceAlternating = alt
-    def add_benefit_points(self, points):
+    def set_benefit_points(self, points):
         self.givePoints = points
-    def add_benefit_military(self, military):
+    def set_benefit_military(self, military):
         self.giveMilitary = military
-    def add_benefit_money(self, money):
+    def set_benefit_money(self, money):
         self.giveMoney = money
-    def add_benefit_research(self, research):
+    def set_benefit_research(self, research):
         self.giveResearch = research
