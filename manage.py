cb39c26c6bd69ca2b64b68cdcb49237e8dc90702
Sam-Irv <samuelkyleirvine@gmail.com>
2017-08-11 12:12:57 +1200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from application.models.test_cards import db_load_test_cards
+from application.models.cards_database import db_populate_cards
 from application.app import app, db
 
 migrate = Migrate(app, db)
@@ -16,7 +16,7 @@ def create_db():
     """Creates the db tables."""
     db.drop_all()
     db.create_all()
-    db_load_test_cards(app, db)
+    db_populate_cards(app, db)
 
 if __name__ == '__main__':
     manager.run()
