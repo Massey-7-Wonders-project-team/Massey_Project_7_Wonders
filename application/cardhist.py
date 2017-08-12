8ce6a6359510316d7523ac2d0b6efde17195416f
jacobstringer <jacobstringer@windowslive.com>
2017-08-13 10:40:51 +1200

    Delete cardhist.py

--- application/cardhist.py
+++
@@ -1,9 +0,0 @@
-from index import db
-from .player import Player
-from .card import Card
-
-
-class Cardhist(db.Model):
-    __tablename__ = 'cardhist'
-    playerId = db.Column(db.Integer, db.ForeignKey('player.id'), primary_key=True)
-    cardId = db.Column(db.Integer, db.ForeignKey('card.id'), primary_key=True)
