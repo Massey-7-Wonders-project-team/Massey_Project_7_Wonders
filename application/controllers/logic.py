ea48ea193b69137a027ed6e17b059337b9108d65
jacobstringer <jacobstringer@windowslive.com>
2017-08-13 18:49:52 +1200

    Add files via upload

--- application/controllers/logic.py
+++ application/controllers/logic.py
@@ -18,7 +18,7 @@ def deal_hands(age, players):
             db.add(dealt_card)
     db.commit_all()
 
-
+"""
 def check_move (card, player, is_discarded, for_wonder):
 
     if is_discarded:
@@ -27,41 +27,47 @@ def check_move (card, player, is_discarded, for_wonder):
     if for_wonder:
         # check if wonder requirements are met
 
+    if (card.costStone > player.stone or
+        card.costBrick > player.brick or
+        card.costOre > player.ore or
+        card.costWood > player.wood or
+        card.costGlass > player.glass or
+        card.costPaper > player.paper or
+        card.costCloth > player.cloth or
+        card.costMoney > player.money):
+        return False
     else:
-        if (card.costStone > player.stone or
-            card.costBrick > player.brick or
-            card.costOre > player.ore or
-            card.costWood > player.wood or
-            card.costGlass > player.glass or
-            card.costPaper > player.paper or
-            card.costCloth > player.cloth or
-            card.costMoney > player.money):
-            return False
-        else:
-            return True
-
+        return True
+"""
 
 def process_card(card, player, is_discarded, for_wonder):
     # checks move is valid, returns false if not
-    if not check_move(card, player, is_discarded, for_wonder):
-        return False
+    #if not check_move(card, player, is_discarded, for_wonder):
+    #    return False
+    if is_discarded:
+        player.money += 3
 
-    if card.colour == 'brown':
-        player.brick += card.giveBrick
-        player.ore += card.giveOre
-        player.wood += card.giveWood
-        player.stone += card.giveStone
+        history = Cardhist(playerId=player.id, cardId=card.id, discarded=True)
+        db.add(history)
 
-    if card.colour == 'grey':
-        player.paper += card.givePaper
-        player.cloth += card.giveCloth
-        player.glass += card.giveGlass
+    else:
+        if card.colour == 'brown':
+            player.brick += card.giveBrick
+            player.ore += card.giveOre
+            player.wood += card.giveWood
+            player.stone += card.giveStone
+        if card.colour == 'grey':
+            player.paper += card.givePaper
+            player.cloth += card.giveCloth
+            player.glass += card.giveGlass
+        if card.colour == 'red':
+            player.military += card.giveMilitary
+        player.money += card.giveMoney - card.costMoney
+        player.points += card.givePoints
 
-    if card.colour == 'red':
-        player.military += card.giveMilitary
+        history = Cardhist(playerId=player.id, cardId=card.id, discarded=False)
+        db.add(history)
 
-    player.money += card.giveMoney - card.costMoney
-    player.points += card.givePoints
 
     #############
     # Update DB #
@@ -71,15 +77,43 @@ def process_card(card, player, is_discarded, for_wonder):
                  .filter_by(playerId=player.id)
                  .filter_by(age=game_info.age)
                  .filter_by(round=game_info.round)).first()
-    players = Player.query.filter_by(gameId=player.gameId).order_by(id).all()
+
+    age = game_info.age
+    round_number = game_info.round
+    old_round.remove(card)
 
     if age == 2:
-        # right
+        next_player = player.right_id
     else:
-        # left
+        next_player = player.left_id
 
-    history = Cardhist(player.id, card.id)
-    db.add(history)
+    for unplayed in old_round:
+        next_round = round(playerId=next_player, age=age, round=round_number+1, cardId=unplayed.id)
+        db.add(next_round)
+
     db.add(player)
+    db.commit_all()
+
+
+    #########################
+    # TURN COMPLETION LOGIC #
+    #########################
+    players = (Player.query.filter_by(gameId=player.gameId)).all()
+    for p in players:
+        if Round.query.filter_by(age=age, round=round_number+1, playerId=p.id) is None:
+            return
+
+    # Only triggers if all players have finished their turn
+    game_info.round += 1
+    if game_info.round == 7:
+        game_info.round = 1
+        game_info.age += 1
+        if game_info.age == 4:
+            game_info.complete = True
+            # TODO FINAL CALCULATIONS FOR END GAME
+        else:
+            deal_hands(game_info.age, players)
+
+    db.add(game_info)
     db.commit_all()
 
--- application/controllers/state.py
+++ application/controllers/state.py
@@ -2,15 +2,8 @@ from ..models.card import Card
 from ..models.player import Player
 from flask import jsonify
 
-class State:
-    player = None
-    cards = []
-
-    def __init__(self, player):
-        self.player = player
-
-def print_json (self, state):
+def print_json (player, cards):
     return {
-        'player': state.player.serialise(),
-        'cards': [card.serialise() for card in state.cards]
+        'player': player.serialise(),
+        'cards': [card.serialise() for card in cards]
     }
