8117b0a6504170c5ba35d6b66b8747a05f9cd06d
Marthijn-B <30314840+Marthijn-B@users.noreply.github.com>
2017-10-02 22:16:18 +1300

    How To play Info
    Added expandable cards for each section
    Original manual PDF
    Sample Screen shot - in lieu of creating proper ones explaining how to navigate through our online game (Just a place holder to show what it can look like

--- static/src/components/Instructions.js
+++ static/src/components/Instructions.js
@@ -1,8 +1,46 @@
 import React from 'react';
+import { Card, CardText, CardHeader, CardMedia } from 'material-ui';
 
 const Instructions = () => (
     <div className="col-md-8">
-        <h1>Instructions</h1>
+        <h1>How to play...</h1>
+        <p> Below are some instructions on how to play 7 Wonders</p>
+        <p> From original game manual to navigating around our <i>Online Capstone Edition 2017</i> </p>
+        <br />
+        <Card style={{ width: 800 }}>
+            <CardHeader
+                title="Original Game Manual"
+                actAsExpander={true}
+                showExpandableButton={true}
+            />
+            <CardText expandable={true} style={{ height: 450 }}>
+                <CardMedia>
+                    <embed
+                        alt='Original Manual PDF'
+                        src='dist/images/background/manual.pdf'
+                        style={{ height: 400 }}
+                    />
+                </CardMedia>
+            </CardText>
+        </Card>
+        <br />
+        <Card style={{ width: 800 }}>
+            <CardHeader
+                title="ScreenShots"
+                actAsExpander={true}
+                showExpandableButton={true}
+            />
+            <CardText expandable={true} style={{ height: 450 }}>
+                <p> Please Note: Proper Screen shots to come. Below is an example </p>
+                <CardMedia>
+                    <embed
+                        alt='Screen Capture - Game Screen'
+                        src='dist/images/background/capture.jpg'
+                        style={{ height: 400 }}
+                    />
+                </CardMedia>
+            </CardText>
+        </Card>
     </div>
 );
 
