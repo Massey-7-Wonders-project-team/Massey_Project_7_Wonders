47b31647b93e3d01ee52bf73042e7a1b1e0c3a3e
Marthijn-B <30314840+Marthijn-B@users.noreply.github.com>
2017-10-16 19:35:01 +1300

    Fixed History array in dummy data
    Should clear one test failure

--- static/tests/components/playerDisplay.test.js
+++ static/tests/components/playerDisplay.test.js
@@ -18,7 +18,7 @@ describe('Component: Player Display', () => {
         },
             game: { age: 1, completed: false, id: 1, round: 1, started: true },
             player: { wonder_level: 1, userId: 1, points: 1, military: 1, profile: 'a', wonder: 'The Temple of Artemis in Ephesus', wood: 0, blue: 0, brick: 0, brown: 0, cloth: 0, glass: 0, paper: 0, cog: 0, ore: 0, stone: 0, compass: 0, extra_ore: 0, extra_brick: 0, extra_cloth: 0, extra_wood: 0, extra_glass: 0, extra_stone: 0, extra_paper: 0, military_loss: 0, tablet: 0, left_id: 2, right_id: 3 },
-            history: [{ card_colour: "wonder", card_name: "ephesus_0", playerId: 1 }],
+            history: [[{ card_colour: "wonder", card_name: "ephesus_0", playerId: 1 }]],
             wonder: { id: 1, name: 'a', slots: 0, card_0: 'a' }
         },
     };
