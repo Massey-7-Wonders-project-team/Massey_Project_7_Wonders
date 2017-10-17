e62b00a119320e5f2903080bdb2e984017a62bef
Marthijn-B <30314840+Marthijn-B@users.noreply.github.com>
2017-10-17 20:15:09 +1300

    fixed 5vp image at end of Age 3
    Was still rendering 3vp - as Henning found

--- static/src/components/MilitaryUpdate.js
+++ static/src/components/MilitaryUpdate.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { Card, CardMedia, CardText } from 'material-ui';
+import { Card, CardMedia, CardText, CardTitle } from 'material-ui';
 
 function search(searchID, allPlayers) {
     const myArray = allPlayers;
@@ -26,18 +26,20 @@ function MilitaryUpdate(props) {
     const levels = militaryData.militaryLevel;
     const names = militaryData.militaryUsers;
     let age = 3;
-    if (!props.data.game.clearInterval) { age = props.data.game.age - 1; }
+    if (!props.data.game.clearInterval && props.data.game.age !== 3) {
+      age = props.data.game.age - 1;
+    }
     let lArmy = [];
-    for (var each = 0; each < levels[0]; each ++) {
-        lArmy.push('militaryUnit');
+    for (let each = 0; each < levels[0]; each ++) {
+        lArmy.push(`leftMilitaryUnit${each}`);
     }
     let uArmy = [];
-    for (var each = 0; each < levels[1]; each ++) {
-        uArmy.push('militaryUnit');
+    for (let each = 0; each < levels[1]; each ++) {
+        uArmy.push(`userMilitaryUnit${each}`);
     }
     let rArmy = [];
-    for (var each = 0; each < levels[2]; each ++) {
-        rArmy.push('militaryUnit');
+    for (let each = 0; each < levels[2]; each ++) {
+        rArmy.push(`rightMilitaryUnit${each}`);
     }
 
     let winLeft = false;
@@ -49,30 +51,30 @@ function MilitaryUpdate(props) {
     if (levels[0] === levels[1]) { evenLeft = true; }
     if (levels[1] === levels[2]) { evenRight = true; }
 
-    let winImage = 1;
+    let winImage = 5;
     if (age === 2) { winImage = 3; }
-    if (age === 3) { winImage = 5; }
+    if (age === 1) { winImage = 1; }
 
     return (
         <div style={{ width: '100%' }}>
             <center>
-                <Card style={{ width: 130, height: '30%', display: 'inline-block', float: 'left' }}>
+                <Card style={{ width: 130, display: 'inline-block', float: 'left' }}>
+                    <CardText>
+                        <h4><center>{names[0]}</center></h4>
+                    </CardText>
                     <CardMedia>
-                        <div style={{ padding: 15, height: 100 }}>
+                        <div style={{ padding: 15 }}>
                             <center>
                                 {
                                   lArmy.map((level) => {
                                       return (
-                                          <img width="30px" alt={level} src={`dist/images/icons/military.png`} />
+                                          <img key={level} width="30px" alt={level} src={`dist/images/icons/military.png`} />
                                       );
                                   })
                                 }
                             </center>
                         </div>
                     </CardMedia>
-                    <CardText>
-                        <h4><center>{names[0]}</center></h4>
-                    </CardText>
                 </Card>
                 <div style={{ width: 170, float: 'left', padding: 50 }}>
                     { !evenLeft ?
@@ -93,23 +95,23 @@ function MilitaryUpdate(props) {
                         </div>
                     }
                 </div>
-                <Card style={{ width: 130, height: '30%', display: 'inline-block', float: 'left' }}>
+                <Card style={{ width: 130, display: 'inline-block', float: 'left' }}>
+                    <CardText>
+                        <h4><center><b>Your Military</b></center></h4>
+                    </CardText>
                     <CardMedia>
-                        <div style={{ padding: 15, height: 100  }}>
+                        <div style={{ padding: 15 }}>
                             <center>
                                 {
                                   uArmy.map((level) => {
                                       return (
-                                          <img width="30px" alt={level} src={`dist/images/icons/military.png`} />
+                                          <img key={level} width="30px" alt={level} src={`dist/images/icons/military.png`} />
                                       );
                                   })
                                 }
                             </center>
                         </div>
                     </CardMedia>
-                    <CardText>
-                        <h4><center><b>Your Military</b></center></h4>
-                    </CardText>
                 </Card>
                 <div style={{ width: 130, float: 'left', padding: 50 }}>
                     { !evenRight ?
@@ -130,23 +132,23 @@ function MilitaryUpdate(props) {
                         </div>
                     }
                 </div>
-                <Card style={{ width: 130, height: '30%', display: 'inline-block', float: 'left' }}>
+                <Card style={{ width: 130, display: 'inline-block', float: 'left' }}>
+                    <CardText>
+                        <h4><center>{names[2]}</center></h4>
+                    </CardText>
                     <CardMedia>
-                        <div style={{ padding: 15, height: 100 }}>
+                        <div style={{ padding: 15 }}>
                             <center>
                                 {
                                   rArmy.map((level) => {
                                       return (
-                                          <img width="30px" alt={level} src={`dist/images/icons/military.png`} />
+                                          <img key={level} width="30px" alt={level} src={`dist/images/icons/military.png`} />
                                       );
                                   })
                                 }
                             </center>
                         </div>
                     </CardMedia>
-                    <CardText>
-                        <h4><center>{names[2]}</center></h4>
-                    </CardText>
                 </Card>
             </center>
         </div>
