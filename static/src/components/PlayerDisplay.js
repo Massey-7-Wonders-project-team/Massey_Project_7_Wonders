558fed4b8811a462314f615d83f4103149e16771
Marthijn-B <30314840+Marthijn-B@users.noreply.github.com>
2017-10-25 23:55:13 +1300

    Display Successful card play messages
    I added this as I haven't utilized the card play success.
    Adds messages between 'Cards in Hand' and 'Player Display' section
    (Last change for this PR- I promise)

--- static/src/components/PlayerDisplay.js
+++ static/src/components/PlayerDisplay.js
@@ -16,6 +16,7 @@ function mapStateToProps(state) {
         error: state.game.error,
         loading: state.game.loading,
         playerCount: state.game.playerCount,
+        message: state.game.message,
     };
 }
 
@@ -31,12 +32,14 @@ export class PlayerDisplay extends Component {
             userID: null,
             displayID: null,
             userData: null,
+            message: null,
         };
         if (this.props.game) {
             // load passed data
             this.state.userID = this.props.game.player.id;
             this.state.displayID = this.props.game.player.id;
             this.state.userData = this.props.game.player;
+            this.state.message = this.props.message;
             // binding
             this.lookLeft = this.lookLeft.bind(this);
             this.lookRight = this.lookRight.bind(this);
@@ -50,7 +53,15 @@ export class PlayerDisplay extends Component {
         if (nextProps.game.player !== this.state.userData) {
             this.setState({
                 userData: nextProps.game.player
-            })
+            });
+        }
+        if (nextProps.message !== null) {
+            if (nextProps.message.includes('played') ||
+              nextProps.message.includes('free')) {
+                this.setState({
+                    message: nextProps.message,
+                });
+            }
         }
     }
 
@@ -126,8 +137,22 @@ export class PlayerDisplay extends Component {
         if (boardData.id === this.state.userID) {
             homeWonder = true;
         }
+        let playedMessage = '';
+        if (this.state.message !== null) {
+            const split1 = this.state.message.split(' ');
+            try {
+                const level = parseInt(split1[0].match(/\d+$/)[0], 10);
+                if (level === 2 || level === 1 || level === 1 || level === 4) {
+                    split1[0] = `Wonder Stage ${level}`;
+                    if (split1[2] === 'played') { split1[2] = 'built';
+                    } else { split1[1] = 'built'; }
+                    playedMessage = split1.join(' ');
+                }
+            } catch (err) {
+                playedMessage = this.state.message;
+            }
+        }
 
-        //overlayContainerStyle={{ paddingBottom: '10%' }}
         return (
             <div style={{ paddingLeft: 100, paddingRight: 100 }}>
                 {game && !error && started && boardData &&
@@ -164,6 +189,9 @@ export class PlayerDisplay extends Component {
                                                                             </List>
                                                                         </TableRowColumn>
                                                                         <TableRowColumn>
+                                                                            <p style={{ margin: 0, paddingLeft: 100 }}><b>Your Last Structure:</b>
+                                                                            <i style={{ paddingLeft: 30 }}>{playedMessage}</i></p>
+
                                                                             <CardMedia
                                                                                 style={{ clear: 'left', paddingTop: 10 }}
                                                                                 overlay={
@@ -235,11 +263,13 @@ export class PlayerDisplay extends Component {
 PlayerDisplay.propTypes = {
     game: PropTypes.object,
     started: PropTypes.bool.isRequired,
+    message: PropTypes.string,
 };
 
 PlayerDisplay.defaultProps = {
     game: null,
     playerCount: null,
+    message: null,
 };
 
 export default connect(
